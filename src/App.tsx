35adde17f1d1850d5f9e2499c6b5f97a0bdac37d
steve levesconte <stevelevesconte@gmail.com>
2023-05-31 14:27:57 -0700

    Add Public Payment page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Footer from "./components/common/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import PublicAccess from "./components/projects/public-access/public-access";
+import PublicPayment from './components/projects/public-payment/public-payment';
 function App() {
   return (<>
     <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/projects/public-access" element={<PublicAccess />} />
+      <Route path="/projects/public-payment" element={<PublicPayment />} />
 
     </Routes>
     <Footer></Footer>
