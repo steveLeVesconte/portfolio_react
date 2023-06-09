d97118070d48c28e0d6b294182916024d5921ccf
steve levesconte <stevelevesconte@gmail.com>
2023-06-09 11:41:45 -0700

    added calendar

---
+++ src/components/mens/calendar.js
@@ -0,0 +1,48 @@
+
+import './calendar.css';
+
+function Calendar() {
+  return (
+    <div className="App">
+      <main>
+        <div className='calendar'>
+          <div class="grid-item">
+            <h3>June 20</h3>
+            <p>s.l.</p>
+            <p>Watch this space.</p>
+          </div>
+          <div class="grid-item">
+            <h3>July 11</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p></div>
+          <div class="grid-item">
+            <h3>July 25</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p>
+          </div>
+          <div class="grid-item">
+            <h3>Aug 1</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p>
+          </div>
+          <div class="grid-item">
+            <h3>Aug 15</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p></div>
+          <div class="grid-item">
+            <h3>Sep 4</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p>
+          </div>
+          <div class="grid-item">
+            <h3>Sep 18</h3>
+            <p>unknown</p>
+            <p>Watch this space.</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Calendar;
